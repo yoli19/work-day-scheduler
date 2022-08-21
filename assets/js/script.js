e47bc84e9826830b0fa95197425284607fbaed9c
Yolanda Jacobs <y.jacobs@hotmail.com>
2022-08-21 01:15:42 -0400

    save task and local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,37 @@ var getCurrentDate = function () {
     $("#currentDay").text(currentDate);
 };
 
+var getRowColors = function () {
+    var currentTime = moment().hour();
+    $('.time-block').each(function () {
+        var rowTime = parseInt($(this).attr("id"));
+        if (rowTime < currentTime) {
+            $(this).children(".description").addClass("past");
+        } else if (rowTime === currentTime) {
+            $(this).children(".description").addClass("present");
+        } else {
+            $(this).children(".description").addClass("future");
+        }
+    });
+};
+
+
+$(".saveBtn").on("click", function () {
+    var taskText = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, taskText);
+});
+
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
 getCurrentDate();
+getRowColors();
